76b3154b6da69de90d0df8946d809f720f9b9c9a
atakanberkyilmaz <atakanberkyilmazz@gmail.com>
2024-07-22 10:09:45 +0200

    tek bir radyo sayfası, main.dart üzerinde çalışıyor

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RadioProvider()),
+      ],
+      child: MaterialApp(
+        home: RadioScreen(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class RadioProvider extends ChangeNotifier {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  String _currentUrl = 'https://limitsiz.kesintisizyayin.com/8002/stream';
+  bool _isPlaying = false;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  String get currentUrl => _currentUrl;
+  bool get isPlaying => _isPlaying;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> playRadio() async {
+    await _audioPlayer.setUrl(_currentUrl);
+    _audioPlayer.play();
+    _isPlaying = true;
+    notifyListeners();
   }
 
+  Future<void> stopRadio() async {
+    await _audioPlayer.stop();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  Future<void> resumeRadio() async {
+    _audioPlayer.play();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  Future<void> togglePlayPause() async {
+    if (_isPlaying) {
+      await stopRadio();
+    } else {
+      await resumeRadio();
+    }
+  }
+
+  Future<void> setVolume(double volume) async {
+    await _audioPlayer.setVolume(volume);
+    notifyListeners();
+  }
+}
+
+class RadioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final radioProvider = Provider.of<RadioProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Radio App'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Image.network(
+              'https://r.resimlink.com/Dcx1mRNUA-Hd.png',
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'NGG Medya',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Sıla Yolu FM',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(Icons.play_arrow),
+                  onPressed: radioProvider.playRadio,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(Icons.stop),
+                  onPressed: radioProvider.stopRadio,
+                ),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(Icons.pause),
+                  onPressed: radioProvider.togglePlayPause,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text('Volume'),
+            Slider(
+              value: radioProvider._audioPlayer.volume,
+              onChanged: (value) {
+                radioProvider.setVolume(value);
+              },
+              min: 0,
+              max: 1,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Playlist',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text(''),
+                  ),
+                  ListTile(
+                    title: Text(''),
+                  ),
+                  ListTile(
+                    title: Text(''),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
