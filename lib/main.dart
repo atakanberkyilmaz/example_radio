cb808aacec5ba3991a3f70a47a4b49d0d2bb1f30
atakanberkyilmaz <atakanberkyilmazz@gmail.com>
2024-07-22 14:25:46 +0200

    awesome_notifications 0.9.3+1 paketi kullanılarak butona basıldığı zaman telefona bildirim gelmesi sağlandı. bunun için AndroidManifest.xml dosyasına izinler eklenildi. izinleri kontrol etmek için ise permission_handler paketi kullanıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:example_radio/radio_provider.dart';
 import 'package:example_radio/screens/home_screen.dart';
 import 'package:example_radio/screens/radio_screen1.dart';
 import 'package:example_radio/screens/radio_screen2.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
-import 'radio_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (await Permission.notification.request().isGranted) {
+    AwesomeNotifications().initialize(
+      'resource://drawable/notification', // Bildirim ikonunu burada belirt res/drawable/...
+      [
+        NotificationChannel(
+          channelKey: 'radio_channel',
+          channelName: 'Radio Notification',
+          channelDescription: 'Bu kanal radio bildirimleri için kullanılıyor',
+          defaultColor: Color(0xFF9D50DD),
+          ledColor: Colors.white,
+          importance: NotificationImportance.Max,
+        ),
+      ],
+    );
+  }
+
   runApp(MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import audio_service
 import audio_session
+import awesome_notifications
+import flutter_local_notifications
 import just_audio
 import path_provider_foundation
 import sqflite
@@ -14,6 +16,8 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
